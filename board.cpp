758129738958217b09d345a7bc84c97818fd2648
Lennart-Go <lennart.n.goertz@gmail.com>
2023-04-26 17:18:29 +0200

    changed initializeBoard that white is at index 0

--- board.cpp
+++ board.cpp
@@ -11,24 +11,27 @@
 // TODO: State information -> King moved, castled?
 
 t_board* initializeBoard() {
-    //black
+
     t_board* board = (t_board*) calloc(1, sizeof(t_board));
+
     board->king     |= (uint64_t) 1 << 4;
     board->queen    |= (uint64_t) 1 << 3;
     board->rook     |= (uint64_t) ROOK_PLACE;
     board->bishop   |= (uint64_t) BISHOP_PLACE;
     board->knight   |= (uint64_t) KNIGHT_PLACE;
     board->pawn     |= (uint64_t) PAWN_PLACE << 8;
-    board->black    |= (uint64_t) COLOR_PLACE << 48;
+    board->white    |= (uint64_t) COLOR_PLACE;
 
-    //white
     board->king     |= (uint64_t) 1 << 60;
     board->queen    |= (uint64_t) 1 << 59;
     board->rook     |= (uint64_t) ROOK_PLACE << 56;
     board->bishop   |= (uint64_t) BISHOP_PLACE << 56;
     board->knight   |= (uint64_t) KNIGHT_PLACE << 56;
     board->pawn     |= (uint64_t) PAWN_PLACE << 48;
-    board->white    |= (uint64_t) COLOR_PLACE;
+    board->black    |= (uint64_t) COLOR_PLACE  << 48;
+
+    board->black |= (uint64_t) 1 << 18;
+    board->pawn |= (uint64_t) 1 << 18;
 
     return board;
 }
