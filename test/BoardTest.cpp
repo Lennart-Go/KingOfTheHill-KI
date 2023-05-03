9cdcecf0626024e5b973665d2c22c6ec1cdb33dc
lcavalar <cavalar@campus.tu-berlin.de>
2023-05-03 16:25:52 +0200

    add BoardTest.cpp

---
+++ test/BoardTest.cpp
@@ -0,0 +1,41 @@
+#include "gtest/gtest.h"
+
+#include "board.h"
+
+using namespace std;
+
+class BoardTest : public ::testing::Test {
+
+protected:
+    virtual void SetUp()
+    {
+        startBoard = initializeBoard();
+    }
+
+    virtual void TearDown()
+    {
+        delete startBoard;
+    }
+
+    t_board *startBoard;
+    char startBoardAsFen[44] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    char wrongBoardAsFen[44] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/ZZZZZZZZ";
+};
+
+
+TEST_F(BoardTest, setFenStartBoard) {
+
+    t_board *board = initializeBoard();
+    setFen(board, startBoardAsFen);
+
+    EXPECT_EQ(startBoard->black, board->black);
+    //EXPECT_ANY_THROW(setFen(board, wrongBoardAsFen)); // no check of wrong input, but not so important
+
+}
+
+TEST_F(BoardTest, getFenStartBoard) {
+
+    char* fen = getFen(startBoard);
+
+    EXPECT_STREQ(fen, startBoardAsFen);
+}
