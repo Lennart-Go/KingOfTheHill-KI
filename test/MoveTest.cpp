e3715b514231a24b1d10d9b2b2a3767ceae6e5d8
Martin Bienert <51266397+embienert@users.noreply.github.com>
2023-05-09 18:03:47 +0200

    Inserted "old" code from local MoveTest.cpp that was overwritten with latest merge.

--- test/MoveTest.cpp
+++ test/MoveTest.cpp
@@ -30,6 +30,152 @@ protected:
 };
 
 
+/*
+ * FUNCTION TESTS FOR empty_between
+ */
+TEST_F(MoveTest, emptyBetweenWhiteVerticalValid) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/Q7");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(0, 2)),
+                   0, 0};
+
+    EXPECT_TRUE(empty_between(board, move));
+}
+
+TEST_F(MoveTest, emptyBetweenWhiteVerticalInvalid) {
+    setFen(board, (char *)"8/8/8/8/8/8/P7/Q7");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(0, 2)),
+                   0, 0};
+
+    EXPECT_FALSE(empty_between(board, move));
+}
+
+TEST_F(MoveTest, emptyBetweenDiagonalValid) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/Q7");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 2)),
+                   0, 0};
+
+    EXPECT_TRUE(empty_between(board, move));
+}
+
+TEST_F(MoveTest, emptyBetweenDiagonalInvalid) {
+    setFen(board, (char *)"8/8/8/8/8/8/1P6/Q7");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 2)),
+                   0, 0};
+
+    EXPECT_FALSE(empty_between(board, move));
+}
+
+TEST_F(MoveTest, emptyBetweenHorizontalValid) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/Q7");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 0)),
+                   0, 0};
+
+    EXPECT_TRUE(empty_between(board, move));
+}
+
+TEST_F(MoveTest, emptyBetweenHorizontalInvalid) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/QP6");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 0)),
+                   0, 0};
+
+    EXPECT_FALSE(empty_between(board, move));
+}
+
+
+/*
+ * FUNCTION TESTS FOR is_move_legal_nocheck
+ */
+TEST_F(MoveTest, isMoveLegalNocheckTargetOccupied) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/Q1P5");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 0)),
+                   0, 0};
+
+    uint64_t whiteColorFilter = board->white;
+    uint64_t blackColorFilter = board->black;
+    bool checkBetweenDefault = false;
+
+    EXPECT_FALSE(is_move_legal_nocheck(board, move, whiteColorFilter, blackColorFilter, checkBetweenDefault));
+}
+
+TEST_F(MoveTest, isMoveLegalNocheckCheckBetweenEmptyBetween) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/Q7");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 0)),
+                   0, 0};
+
+    uint64_t whiteColorFilter = board->white;
+    uint64_t blackColorFilter = board->black;
+
+    EXPECT_TRUE(is_move_legal_nocheck(board, move, whiteColorFilter, blackColorFilter, true));
+}
+
+TEST_F(MoveTest, isMoveLegalNocheckCheckBetweenNotEmptyBetween) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/QP6");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 0)),
+                   0, 0};
+
+    uint64_t whiteColorFilter = board->white;
+    uint64_t blackColorFilter = board->black;
+
+    EXPECT_FALSE(is_move_legal_nocheck(board, move, whiteColorFilter, blackColorFilter, true));
+}
+
+TEST_F(MoveTest, isMoveLegalNocheckNoCheckBetweenEmptyBetween) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/Q7");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 0)),
+                   0, 0};
+
+    uint64_t whiteColorFilter = board->white;
+    uint64_t blackColorFilter = board->black;
+
+    EXPECT_TRUE(is_move_legal_nocheck(board, move, whiteColorFilter, blackColorFilter, false));
+}
+
+TEST_F(MoveTest, isMoveLegalNocheckNoCheckBetweenNotEmptyBetween) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/QP6");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 0)),
+                   0, 0};
+
+    uint64_t whiteColorFilter = board->white;
+    uint64_t blackColorFilter = board->black;
+
+    EXPECT_TRUE(is_move_legal_nocheck(board, move, whiteColorFilter, blackColorFilter, false));
+}
+
+TEST_F(MoveTest, isMoveLegalNocheckValid) {
+    setFen(board, (char *)"8/8/8/8/8/8/8/Q7");
+    t_move move = {(unsigned )shift_from_position(Position(0, 0)),
+                   (unsigned )shift_from_position(Position(2, 0)),
+                   0, 0};
+
+    uint64_t whiteColorFilter = board->white;
+    uint64_t blackColorFilter = board->black;
+
+    EXPECT_TRUE(is_move_legal_nocheck(board, move, whiteColorFilter, blackColorFilter, true));
+}
+
+
+/*
+ * FUNCTION TESTS FOR is_threatened WITH WHITE PIECES
+ */
+// TODO
+
+
+/*
+ * FUNCTION TESTS FOR is_threatened WITH BLACK PIECES
+ */
+
+
 TEST_F(MoveTest, doMove) {
 
     t_board *board = initializeBoard();
